e49c50b205a10e70bc7038a7f2131fa466866b41
Ehab Salah <ehab.salah020@gmail.com>
2021-12-18 18:20:24 +0200

    Initial commit

---
+++ bin/deserialize_union_types.dart
@@ -0,0 +1,5 @@
+import 'package:deserialize_union_types/deserialize_union_types.dart' as deserialize_union_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${deserialize_union_types.calculate()}!');
+}
